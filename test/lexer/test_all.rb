38975469f08e9dc37f348501e5f055525201a2f8
Ammar Ali <ammarabuali@gmail.com>
2010-10-22 10:49:30 +0300

    Removed old comments tests; now covered in group tests

--- test/lexer/test_all.rb
+++ test/lexer/test_all.rb
@@ -1,8 +1,7 @@
 require File.expand_path("../../helpers", __FILE__)
 
 %w{
-  anchors comments escapes groups properties
-  quantifiers sets
+  anchors escapes groups properties quantifiers sets types
 }.each do|tc|
   require File.expand_path("../test_#{tc}", __FILE__)
 end
