e9a372504b401e576dfc374c77081b1eca31d6c4
Ammar Ali <ammarabuali@gmail.com>
2010-10-25 00:03:52 +0300

    No need for "instance" variables anymore

--- lib/regexp_parser/lexer.rb
+++ lib/regexp_parser/lexer.rb
@@ -7,22 +7,22 @@ module Regexp::Lexer
   CLOSE_TOKENS  = [:close]
 
   def self.scan(input, syntax = :any, &block)
-    @depth  = 0
-    @tokens = []
-    @syntax = Regexp::Syntax.find(syntax)
+    depth  = 0
+    tokens = []
+    syntax = Regexp::Syntax.find(syntax)
 
     Regexp::Scanner.scan(input) do |type, token, text, ts, te|
-      @syntax.check! type, token
+      syntax.check! type, token
 
-      @depth -= 1 if CLOSE_TOKENS.include?(token)
-      @tokens << Regexp::Token.new(type, token, text, ts, te, @depth)
-      @depth += 1 if OPEN_TOKENS.include?(token)
+      depth -= 1 if CLOSE_TOKENS.include?(token)
+      tokens << Regexp::Token.new(type, token, text, ts, te, depth)
+      depth += 1 if OPEN_TOKENS.include?(token)
     end
 
     if block_given?
-      @tokens.each {|token| yield token}
+      tokens.each {|token| yield token}
     else
-      @tokens
+      tokens
     end
   end
 
