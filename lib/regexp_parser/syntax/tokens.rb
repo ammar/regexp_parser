7179b106b6b4ef94ef2b47af33aafb04949603e1
Ammar Ali <ammarabuali@gmail.com>
2010-10-26 20:07:18 +0300

    Added initial token collections

---
+++ lib/regexp_parser/syntax/tokens.rb
@@ -0,0 +1,54 @@
+module Regexp::Syntax
+
+  module Token
+    module Meta
+      Basic     = [:dot]
+      Extended  = Basic + [:alternation]
+    end
+
+    module Anchor
+      Basic     = [:beginning_of_line, :end_of_line]
+      Extended  = Basic + [:word_boundary, :nonword_boundary]
+      String    = [:bos, :eos, :eos_ob_eol]
+    end
+
+    module Group
+      Basic     = [:capture, :close]
+      Extended  = Basic + []
+    end
+
+    module CharacterType
+      Basic     = []
+      Extended  = [:digit, :nondigit, :hex, :nonhex, :space, :nonspace,
+                   :word, :nonword]
+    end
+
+    module CharacterSet
+       # TODO: break apart
+      Basic     = [:open, :close, :negate, :member, :range]
+      Extended  = Basic + [:escape, :intersection, :range_hex, :backspace]
+
+      Types     = [:type_digit, :type_nondigit, :type_hex, :type_nonhex,
+                   :type_space, :type_nonspace, :type_word, :type_nonword]
+    end
+
+    module Quantifier
+      Greedy      = [:zero_or_one, :zero_or_more, :one_or_more]
+      Reluctant   = [:zero_or_one_reluctant, :zero_or_more_reluctant, :one_or_more_reluctant]
+      Possessive  = [:zero_or_one_possessive, :zero_or_more_possessive, :one_or_more_possessive]
+      Interval    = [:interval]
+    end
+
+    module Escape
+      Backreference = [:digit]
+
+      ASCII = [:bell, :backspace, :escape, :form_feed, :newline, :carriage,
+               :space, :tab, :vertical_tab]
+
+      Meta  = [:dot, :alternation, :zero_or_one, :zero_or_more, :one_or_more,
+               :beginning_of_line, :end_of_line, :group_open, :group_close,
+               :interval_open, :interval_close, :set_open, :set_close, :baclslash]
+    end
+  end
+
+end
