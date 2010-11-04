3ec844aa269a546790f25e7dcbc87c596c16a50b
Ammar Ali <ammarabuali@gmail.com>
2010-11-04 15:57:05 +0200

    More tests cleanup

--- test/parser/test_all.rb
+++ test/parser/test_all.rb
@@ -1,7 +1,7 @@
 require File.expand_path("../../helpers", __FILE__)
 
 %w{
-  anchors comments escapes groups properties
+  alternation anchors escapes groups properties
   quantifiers sets
 }.each do|tc|
   require File.expand_path("../test_#{tc}", __FILE__)
--- test/parser/test_groups.rb
+++ test/parser/test_groups.rb
@@ -1,15 +1,25 @@
 require File.expand_path("../../helpers", __FILE__)
 
-class TestRegexpParserGroups < Test::Unit::TestCase
+class TestParserGroups < Test::Unit::TestCase
 
-  def test_parse_group
-    #t = RP.parse('(?ix-mx:a(?<name>b*(?:c*(?#d+))))')
+  def test_parse_options
     t = RP.parse(/(?xi-m:a(?m-ix:b))/)
-    #puts " *** ROOT: #{t.inspect}"
 
     assert_equal( false, t.expressions.first.m? )
     assert_equal( true, t.expressions.first.i? )
     assert_equal( true, t.expressions.first.x? )
+  end
+
+  def test_parse_comment
+    t = RP.parse('a(?# is for apple)b(?# for boy)c(?# cat)')
+
+    [1,3,5].each do |i|
+      assert( t.expressions[i].is_a?(Group::Comment),
+             "Expected comment, but got #{t.expressions[i].class.name}")
+
+      assert_equal( :group,   t.expressions[i].type )
+      assert_equal( :comment, t.expressions[i].token )
+    end
   end
 
 end
