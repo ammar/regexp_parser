28ac6222f181622c9617b1eb7fd63e87bb20193a
Ammar Ali <ammarabuali@gmail.com>
2010-11-12 02:19:26 +0200

    Updated usage note in multitest script

--- test/multitest.sh
+++ test/multitest.sh
@@ -1,15 +1,16 @@
 # Poor man's multiruby, a wrapper bash function/script around rvm. This is
 # used to run all the tests through all the supported versions of ruby.
 #
-# Requires that all the rubies listed below are installed via rvm, and rvm to
-# be sourced in the environment.
+# Requires that all the rubies listed below are installed via rvm. rvm gets
+# sourced by the function.
 #
-# Usage: ./multitest
+# Usage: ./test/multitest.sh
 
 multitest() {
   versions=('1.8.6' '1.8.7' '1.9.1' '1.9.2')
 
   source $HOME/.rvm/scripts/rvm
+
   echo "Running tests for ${#versions[@]} ruby versions"
   echo "------------------------------------------------------------"
   for version in ${versions[@]}
@@ -18,7 +19,9 @@ multitest() {
     echo `ruby --version`
     rake > test/run/${version}.run 2> test/run/${version}.run
 
-    tail -n 3 test/run/${version}.run | egrep "[[:digit:]]+ tests, [[:digit:]]+ assertions"
+    tail -n 3 test/run/${version}.run |
+      egrep "[[:digit:]]+ tests, [[:digit:]]+ assertions"
+
     echo "------------------------------------------------------------"
   done
 }
