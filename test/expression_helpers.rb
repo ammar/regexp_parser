be246b56bcfd07c11a7aece96edb3ea086ea4230
Ammar Ali <ammarabuali@gmail.com>
2013-02-26 20:03:40 +0200

    Delete useless cruft.

--- test/expression_helpers.rb
+++ test/expression_helpers.rb
@@ -8,34 +8,3 @@ RR = Regexp::Runner
 RM = Regexp::Matcher
 
 include Regexp::Expression
-
-module Regexp::Expression
-  module Scope
-    Any = { :* => :* }
-
-    All = {
-      :anchor      => [:*],
-      :literal     => [:*],
-      :assertion   => [:*],
-      :backref     => [:*],
-      :set         => [:*],
-      :subset      => [:*],
-      :type        => [:*],
-      :escape      => [:*],
-      :group       => [:*],
-      :meta        => [:*],
-      :property    => [:*],
-      :nonproperty => [:*],
-      :expression  => [:*],
-    }
-  end
-
-  class Base
-  end
-
-  class Subexpression
-  end
-
-  class Root
-  end
-end
