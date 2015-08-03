e73531ef2d47c11369d080fadafb1f176ed8e3e3
Garen Torikian <gjtorikian@gmail.com>
2015-08-02 22:01:14 -0700

    Add UnicodeBlock tests

--- test/scanner/test_all.rb
+++ test/scanner/test_all.rb
@@ -2,7 +2,7 @@ require File.expand_path("../../helpers", __FILE__)
 
 %w{
   anchors errors escapes free_space groups literals
-  meta properties quantifiers scripts sets types
+  meta properties quantifiers scripts sets types unicode_blocks
 }.each do|tc|
   require File.expand_path("../test_#{tc}", __FILE__)
 end
