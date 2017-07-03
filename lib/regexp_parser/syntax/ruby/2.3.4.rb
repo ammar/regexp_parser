d487f15c587c1bb2dbb686d511db069d5c6910ae
Bartek Bułat <bartek.bulat@gmail.com>
2017-07-03 17:01:26 +0200

    Add support for Ruby 2.3.4 version
     - Adds ruby version files file for ruby 2.3.4

---
+++ lib/regexp_parser/syntax/ruby/2.3.4.rb
@@ -0,0 +1,13 @@
+require File.expand_path('../2.3.3', __FILE__)
+
+module Regexp::Syntax
+  module Ruby
+
+    class V234 < Regexp::Syntax::Ruby::V233
+      def initialize
+        super
+      end
+    end
+
+  end
+end
