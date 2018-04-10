3fd0f17602d2f475837b529a0e0595109eb81c56
Janosch Müller <janosch84@gmail.com>
2018-04-10 20:44:33 +0200

    Simplify Regexp::Syntax::Base quite a bit

--- lib/regexp_parser/syntax/base.rb
+++ lib/regexp_parser/syntax/base.rb
@@ -1,5 +1,6 @@
-module Regexp::Syntax
+require 'set'
 
+module Regexp::Syntax
   class NotImplementedError < SyntaxError
     def initialize(syntax, type, token)
       super "#{syntax.class.name} does not implement: [#{type}:#{token}]"
@@ -8,6 +9,8 @@ module Regexp::Syntax
 
   # A lookup map of supported types and tokens in a given syntax
   class Base
+    include Regexp::Syntax::Token
+
     def initialize
       @implements = {}
 
@@ -15,37 +18,20 @@ module Regexp::Syntax
       implements Token::FreeSpace::Type, Token::FreeSpace::All
     end
 
-    def implementation
-      @implements
+    def implementations(type)
+      @implements[type] ||= Set.new
     end
 
     def implements(type, tokens)
-      if @implements[type]
-        @implements[type] = (@implements[type] + tokens).uniq
-      else
-        @implements[type] = tokens
-      end
+      implementations(type).merge(Array(tokens))
     end
 
-    # removes
     def excludes(type, tokens)
-      if tokens
-        tokens = [tokens] unless tokens.is_a?(Array)
-      end
-
-      if @implements[type]
-        if tokens
-          @implements[type] = @implements[type] - tokens
-          @implements[type] = nil if @implements[type].empty?
-        else
-          @implements[type] = nil
-        end
-      end
+      implementations(type).subtract(Array(tokens))
     end
 
     def implements?(type, token)
-      return true if @implements[type] and @implements[type].include?(token)
-      false
+      implementations(type).include?(token)
     end
     alias :check? :implements?
 
@@ -102,5 +88,4 @@ module Regexp::Syntax
       "#{super} (feature set of #{ancestors[1].to_s.split('::').last})"
     end
   end
-
 end
