210124418ce33c5e8d3093a750e54865c2c2eec3
Janosch Müller <janosch84@gmail.com>
2018-08-29 20:16:44 +0200

    Remove LegacyCompatibilityMethods

--- lib/regexp_parser/expression/classes/set.rb
+++ lib/regexp_parser/expression/classes/set.rb
@@ -1,5 +1,4 @@
 module Regexp::Expression
-
   class CharacterSet < Regexp::Expression::Subexpression
     attr_accessor :closed, :negative
 
@@ -24,50 +23,5 @@ module Regexp::Expression
     def to_s(format = :full)
       "#{text}#{'^' if negated?}#{expressions.join}]#{quantifier_affix(format)}"
     end
-
-    # TODO: these made more sense with string members. remove/replace in v1.0.0?
-    module LegacyCompatibilityMethods
-      def members
-        expressions.map { |exp| exp.is_a?(CharacterSet) ? exp : exp.to_s }
-      end
-
-      # Returns an array of the members with any shorthand members like \d and \W
-      # expanded to either traditional form or unicode properties.
-      def expand_members(use_properties = false)
-        members.map do |member|
-          case member
-          when "\\d"
-            use_properties ? '\p{Digit}'  : '0-9'
-          when "\\D"
-            use_properties ? '\P{Digit}'  : '^0-9'
-          when "\\w"
-            use_properties ? '\p{Word}'   : 'A-Za-z0-9_'
-          when "\\W"
-            use_properties ? '\P{Word}'   : '^A-Za-z0-9_'
-          when "\\s"
-            use_properties ? '\p{Space}'  : ' \t\f\v\n\r'
-          when "\\S"
-            use_properties ? '\P{Space}'  : '^ \t\f\v\n\r'
-          when "\\h"
-            use_properties ? '\p{Xdigit}' : '0-9A-Fa-f'
-          when "\\H"
-            use_properties ? '\P{Xdigit}' : '^0-9A-Fa-f'
-          else
-            member
-          end
-        end
-      end
-
-      def include?(member, directly = false)
-        members.any? do |m|
-          if m.is_a?(CharacterSet)
-            !directly && m.include?(member)
-          else
-            m == member
-          end
-        end
-      end
-    end
-    include LegacyCompatibilityMethods
   end
 end # module Regexp::Expression
