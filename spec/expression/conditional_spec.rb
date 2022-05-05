49890bbaf3839de85c39cda996a97f8a78e704ab
Janosch Müller <janosch.mueller@betterplace.org>
2022-05-05 22:06:10 +0200

    Standardize conditionals spec

--- spec/expression/conditional_spec.rb
+++ spec/expression/conditional_spec.rb
@@ -1,89 +1,22 @@
 require 'spec_helper'
 
 RSpec.describe(Regexp::Expression::Conditional) do
-  let(:root)   { RP.parse('^(a(b))(b(?(1)c|(?(2)d|(?(3)e|f)))g)$') }
-  let(:cond_1) { root.dig(2, 1) }
-  let(:cond_2) { root.dig(2, 1, 2, 0) }
-  let(:cond_3) { root.dig(2, 1, 2, 0, 2, 0) }
-
-  specify('root level') do
-    [
-      '^',
-      '(a(b))',
-      '(b(?(1)c|(?(2)d|(?(3)e|f)))g)',
-      '$'
-    ].each_with_index do |t, i|
-      expect(root.dig(i).conditional_level).to eq 0
-      expect(root.dig(i).to_s).to eq t
-    end
-
-    expect(root.dig(2, 0).to_s).to eq 'b'
-    expect(root.dig(2, 0).conditional_level).to eq 0
+  specify('Conditional#condition, #branches') do
+    conditional = RP.parse(/(?<A>a)(?(<A>)T|F)/)[1]
+    expect(conditional.condition).to eq conditional[0]
+    expect(conditional.branches).to eq conditional[1..2]
   end
 
-  specify('level one') do
-    condition = cond_1.condition
-    branch_1 = cond_1.branches.first
-
-    expect(condition).to be_a Conditional::Condition
-    expect(condition.to_s).to eq '(1)'
-    expect(condition.conditional_level).to eq 1
-
-    expect(branch_1).to be_a Conditional::Branch
-    expect(branch_1.to_s).to eq 'c'
-    expect(branch_1.conditional_level).to eq 1
-
-    expect(branch_1.first.to_s).to eq 'c'
-    expect(branch_1.first.conditional_level).to eq 1
+  specify('Condition#referenced_expression') do
+    exp = RP.parse(/(?<A>a)(?(<A>)T|F)/)[1].condition
+    expect(exp.referenced_expression.to_s).to eq '(?<A>a)'
+    exp = RP.parse(/(a)(?(1)T|F)/)[1].condition
+    expect(exp.referenced_expression.to_s).to eq '(a)'
   end
 
-  specify('level two') do
-    condition = cond_2.condition
-    branch_1 = cond_2.branches.first
-    branch_2 = cond_2.branches.last
+  specify('parse conditional excessive branches') do
+    regexp = '(?<A>a)(?(<A>)T|F|X)'
 
-    expect(cond_2.to_s).to start_with '(?'
-    expect(cond_2.conditional_level).to eq 1
-
-    expect(condition).to be_a Conditional::Condition
-    expect(condition.to_s).to eq '(2)'
-    expect(condition.conditional_level).to eq 2
-
-    expect(branch_1).to be_a Conditional::Branch
-    expect(branch_1.to_s).to eq 'd'
-    expect(branch_1.conditional_level).to eq 2
-
-    expect(branch_1.first.to_s).to eq 'd'
-    expect(branch_1.first.conditional_level).to eq 2
-
-    expect(branch_2.first.to_s).to start_with '(?'
-    expect(branch_2.first.conditional_level).to eq 2
-  end
-
-  specify('level three') do
-    condition = cond_3.condition
-    branch_1 = cond_3.branches.first
-    branch_2 = cond_3.branches.last
-
-    expect(condition).to be_a Conditional::Condition
-    expect(condition.to_s).to eq '(3)'
-    expect(condition.conditional_level).to eq 3
-
-    expect(cond_3.to_s).to eq '(?(3)e|f)'
-    expect(cond_3.conditional_level).to eq 2
-
-    expect(branch_1).to be_a Conditional::Branch
-    expect(branch_1.to_s).to eq 'e'
-    expect(branch_1.conditional_level).to eq 3
-
-    expect(branch_1.first.to_s).to eq 'e'
-    expect(branch_1.first.conditional_level).to eq 3
-
-    expect(branch_2).to be_a Conditional::Branch
-    expect(branch_2.to_s).to eq 'f'
-    expect(branch_2.conditional_level).to eq 3
-
-    expect(branch_2.first.to_s).to eq 'f'
-    expect(branch_2.first.conditional_level).to eq 3
+    expect { RP.parse(regexp) }.to raise_error(Conditional::TooManyBranches)
   end
 end
--- spec/parser/conditionals_spec.rb
+++ spec/parser/conditionals_spec.rb
@@ -1,179 +1,69 @@
 require 'spec_helper'
 
 RSpec.describe('Conditional parsing') do
-  specify('parse conditional') do
-    regexp = /(?<A>a)(?(<A>)T|F)/
+  include_examples 'parse', /(?<A>a)(?(<A>)T|F)/,
+    [1]       => [:conditional, :open, Conditional::Expression, to_s: '(?(<A>)T|F)', reference: 'A'],
+    [1, 0]    => [:conditional, :condition, Conditional::Condition, to_s: '(<A>)', reference: 'A'],
+    [1, 1]    => [:expression,  :sequence,  Conditional::Branch, to_s: 'T'],
+    [1, 1, 0] => [:literal, text: 'T'],
+    [1, 2]    => [:expression,  :sequence,  Conditional::Branch, to_s: 'F'],
+    [1, 2, 0] => [:literal, text: 'F']
 
-    root = RP.parse(regexp)
-    exp = root.dig(1)
+  include_examples 'parse', /(a)(?(1)T|F)/,
+    [1]       => [:conditional, :open, Conditional::Expression, to_s: '(?(1)T|F)', reference: 1],
+    [1, 0]    => [:conditional, :condition, Conditional::Condition, to_s: '(1)', reference: 1],
+    [1, 1]    => [:expression,  :sequence,  Conditional::Branch, to_s: 'T'],
+    [1, 1, 0] => [:literal, text: 'T'],
+    [1, 2]    => [:expression,  :sequence,  Conditional::Branch, to_s: 'F'],
+    [1, 2, 0] => [:literal, text: 'F']
 
-    expect(exp).to be_a(Conditional::Expression)
+  include_examples 'parse', /(foo)(?(1)\d+|(\w)){42}/,
+    [1]       => [Conditional::Expression, quantified?: true, to_s: '(?(1)\d+|(\w)){42}'],
+    [1, 0]    => [Conditional::Condition, quantified?: false],
+    [1, 1]    => [Conditional::Branch, quantified?: false],
+    [1, 1, 0] => [:digit, quantified?: true, to_s: '\d+'],
+    [1, 2]    => [Conditional::Branch, quantified?: false]
 
-    expect(exp.type).to eq :conditional
-    expect(exp.token).to eq :open
-    expect(exp.to_s).to eq '(?(<A>)T|F)'
-    expect(exp.reference).to eq 'A'
-  end
+  # test nested and mixed with alternations
+  include_examples 'parse', <<-EOS.gsub(/\s/, ''),
+      (
+        (a)
+        |
+        (b)
+        |
+        (
+          (
+            ?(2)
+            (c(d|e)+)?
+            |
+            (
+              ?(3)
+              f
+              |
+              (
+                ?(4)
+                (g|(h)(i))
+              )
+            )
+          )
+        )
+      )
+    EOS
+    [0]                         => [Group::Capture, count: 1],
+    [0, 0]                      => [Alternation, count: 3],
+    [0, 0, 2]                   => [Alternative, count: 1],
+    [0, 0, 2, 0]                => [Group::Capture, count: 1],
+    [0, 0, 2, 0, 0]             => [Conditional::Expression, count: 3, conditional_level: 0],
+    [0, 0, 2, 0, 0, 0]          => [Conditional::Condition, to_s: '(2)', conditional_level: 1],
+    [0, 0, 2, 0, 0, 1]          => [Conditional::Branch, to_s: '(c(d|e)+)?', conditional_level: 1],
+    [0, 0, 2, 0, 0, 2]          => [Conditional::Branch, to_s: '(?(3)f|(?(4)(g|(h)(i))))', conditional_level: 1],
+    [0, 0, 2, 0, 0, 2, 0]       => [Conditional::Expression, count: 3, conditional_level: 1],
+    [0, 0, 2, 0, 0, 2, 0, 0]    => [Conditional::Condition, to_s: '(3)', conditional_level: 2],
+    [0, 0, 2, 0, 0, 2, 0, 1]    => [Conditional::Branch, count: 1, to_s: 'f', conditional_level: 2],
+    [0, 0, 2, 0, 0, 2, 0, 1, 0] => [Literal, text: 'f', conditional_level: 2]
 
-  specify('parse conditional condition') do
-    regexp = /(?<A>a)(?(<A>)T|F)/
-
-    root = RP.parse(regexp)
-    exp = root.dig(1).condition
-
-    expect(exp).to be_a(Conditional::Condition)
-
-    expect(exp.type).to eq :conditional
-    expect(exp.token).to eq :condition
-    expect(exp.to_s).to eq '(<A>)'
-    expect(exp.reference).to eq 'A'
-    expect(exp.referenced_expression.to_s).to eq '(?<A>a)'
-  end
-
-  specify('parse conditional condition with number ref') do
-    regexp = /(a)(?(1)T|F)/
-
-    root = RP.parse(regexp)
-    exp = root.dig(1).condition
-
-    expect(exp).to be_a(Conditional::Condition)
-
-    expect(exp.type).to eq :conditional
-    expect(exp.token).to eq :condition
-    expect(exp.to_s).to eq '(1)'
-    expect(exp.reference).to eq 1
-    expect(exp.referenced_expression.to_s).to eq '(a)'
-  end
-
-  specify('parse conditional nested groups') do
-    regexp = /((a)|(b)|((?(2)(c(d|e)+)?|(?(3)f|(?(4)(g|(h)(i)))))))/
-
-    root = RP.parse(regexp)
-
-    expect(root.to_s).to eq regexp.source
-
-    group = root.first
-    expect(group).to be_instance_of(Group::Capture)
-
-    alt = group.first
-    expect(alt).to be_instance_of(Alternation)
-    expect(alt.length).to eq 3
-
-    expect(alt.map(&:first)).to all(be_a Group::Capture)
-
-    subgroup = alt[2].first
-    conditional = subgroup.first
-
-    expect(conditional).to be_instance_of(Conditional::Expression)
-    expect(conditional.length).to eq 3
-
-    expect(conditional[0]).to be_instance_of(Conditional::Condition)
-    expect(conditional[0].to_s).to eq '(2)'
-
-    condition = conditional.condition
-    expect(condition).to be_instance_of(Conditional::Condition)
-    expect(condition.to_s).to eq '(2)'
-
-    branches = conditional.branches
-    expect(branches.length).to eq 2
-    expect(branches).to be_instance_of(Array)
-  end
-
-  specify('parse conditional nested') do
-    regexp = /(a(b(c(d)(e))))(?(1)(?(2)d|(?(3)e|f))|(?(4)(?(5)g|h)))/
-
-    root = RP.parse(regexp)
-
-    expect(root.to_s).to eq regexp.source
-
-    {
-      1 => [2, root.dig(1)],
-      2 => [2, root.dig(1, 1, 0)],
-      3 => [2, root.dig(1, 1, 0, 2, 0)],
-      4 => [1, root.dig(1, 2, 0)],
-      5 => [2, root.dig(1, 2, 0, 1, 0)]
-    }.each do |index, example|
-      branch_count, exp = example
-
-      expect(exp).to be_instance_of(Conditional::Expression)
-      expect(exp.condition.to_s).to eq "(#{index})"
-      expect(exp.branches.length).to eq branch_count
-    end
-  end
-
-  specify('parse conditional nested alternation') do
-    regexp = /(a)(?(1)(b|c|d)|(e|f|g))(h)(?(2)(i|j|k)|(l|m|n))|o|p/
-
-    root = RP.parse(regexp)
-
-    expect(root.to_s).to eq regexp.source
-
-    expect(root.first).to be_instance_of(Alternation)
-
-    [
-      [3, 'b|c|d', root.dig(0, 0, 1, 1, 0, 0)],
-      [3, 'e|f|g', root.dig(0, 0, 1, 2, 0, 0)],
-      [3, 'i|j|k', root.dig(0, 0, 3, 1, 0, 0)],
-      [3, 'l|m|n', root.dig(0, 0, 3, 2, 0, 0)]
-    ].each do |example|
-      alt_count, alt_text, exp = example
-
-      expect(exp).to be_instance_of(Alternation)
-      expect(exp.to_s).to eq alt_text
-      expect(exp.alternatives.length).to eq alt_count
-    end
-  end
-
-  specify('parse conditional extra separator') do
-    regexp = /(?<A>a)(?(<A>)T|)/
-
-    root = RP.parse(regexp)
-    branches = root.dig(1).branches
-
-    expect(branches.length).to eq 2
-
-    seq_1, seq_2 = branches
-
-    [seq_1, seq_2].each do |seq|
-      expect(seq).to be_a(Sequence)
-
-      expect(seq.type).to eq :expression
-      expect(seq.token).to eq :sequence
-    end
-
-    expect(seq_1.to_s).to eq 'T'
-    expect(seq_2.to_s).to eq ''
-  end
-
-  specify('parse conditional quantified') do
-    regexp = /(foo)(?(1)\d|(\w)){42}/
-
-    root = RP.parse(regexp)
-    conditional = root.dig(1)
-
-    expect(conditional).to be_quantified
-    expect(conditional.quantifier.to_s).to eq '{42}'
-    expect(conditional.to_s).to eq '(?(1)\d|(\w)){42}'
-    expect(conditional.branches.any?(&:quantified?)).to be false
-  end
-
-  specify('parse conditional branch content quantified') do
-    regexp = /(foo)(?(1)\d{23}|(\w){42})/
-
-    root = RP.parse(regexp)
-    conditional = root.dig(1)
-
-    expect(conditional).not_to be_quantified
-    expect(conditional.branches.any?(&:quantified?)).to be false
-    expect(conditional.branches.dig(0, 0)).to be_quantified
-    expect(conditional.branches.dig(0, 0).quantifier.to_s).to eq '{23}'
-    expect(conditional.branches.dig(1, 0)).to be_quantified
-    expect(conditional.branches.dig(1, 0).quantifier.to_s).to eq '{42}'
-  end
-
-  specify('parse conditional excessive branches') do
-    regexp = '(?<A>a)(?(<A>)T|F|X)'
-
-    expect { RP.parse(regexp) }.to raise_error(Conditional::TooManyBranches)
-  end
+  # test empty branch
+  include_examples 'parse', /(?<A>a)(?(<A>)T|)/,
+    [1]    => [Conditional::Expression, count: 3, to_s: '(?(<A>)T|)'],
+    [1, 2] => [Conditional::Branch, to_s: '']
 end
