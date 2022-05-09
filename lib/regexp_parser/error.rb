b633339c9f121ba8c43227d63606ff86456a8ba7
Janosch Müller <janosch.mueller@betterplace.org>
2022-05-09 21:20:50 +0200

    Correct comment

--- lib/regexp_parser/error.rb
+++ lib/regexp_parser/error.rb
@@ -1,4 +1,4 @@
 class Regexp::Parser
-  # base class for all gem-specific errors (inherited but never raised itself)
+  # base class for all gem-specific errors
   class Error < StandardError; end
 end
